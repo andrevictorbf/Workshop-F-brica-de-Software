edb9507251d950f089122668c10baa67d28e8dc5
andrevictorbf <andrevictor_barcia@hotmail.com>
2023-08-28 17:31:45 -0300

    first commit

---
+++ atividade1.py
@@ -0,0 +1,9 @@
+#Atividade 1 - 1º dia do Workshop da fábrica de software - 28/08/2023
+#Cálculo de número antecessor e sucessor - Há uso dos comandos input - entrada da variável - print - para a impresão na tela do usuário - conjunto de números inteiros - int;
+# O "F" serve para concatenar as variáveis. 
+num = input ("Digite uma variável ")
+
+antecessor = int(num) - 1
+sucessor = int(num) + 1
+
+print (f"O numero digitado foi {num} e seu antecessor é {antecessor} e o seu sucessor é {sucessor}")
